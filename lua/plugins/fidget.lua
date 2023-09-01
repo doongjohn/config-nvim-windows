17edcebd7f6e56dcfee54cf2f4e738a5e88a4a45
doongjohn <doongjohn@gmail.com>
2023-09-02 02:37:15 +0900

    update

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,12 @@
+return {
+  -- lsp spinner
+  'j-hui/fidget.nvim',
+  opts = {
+    text = {
+      spinner = 'dots_snake',
+      done = '',
+      commenced = 'Started',
+      completed = 'Completed',
+    },
+  },
+}
