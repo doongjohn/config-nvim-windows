6692a9920681d19f7005dad4bfee1e265cf076f3
doongjohn <doongjohn@gmail.com>
2023-09-07 21:05:27 +0900

    update

--- init.lua
+++ init.lua
@@ -39,7 +39,7 @@ vim.o.expandtab = true
 
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     'git',
     'clone',
