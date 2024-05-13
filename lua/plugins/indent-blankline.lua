17ca9ee454fba7a81d6c2c5bc6eabc6807448698
doongjohn <doongjohn@gmail.com>
2024-05-13 23:03:11 +0900

    update

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,17 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  event = 'BufEnter',
+  main = 'ibl',
+  config = function()
+    vim.api.nvim_set_hl(0, 'IblIndent', { link = 'LineNr' })
+    require 'ibl'.setup {
+      indent = {
+        highlight = 'IblIndent',
+        char = '│',
+      },
+      scope = {
+        enabled = false,
+      },
+    }
+  end,
+}
