3bff5f557bb579e4d1d5bd64b94c75f04426fabe
doongjohn <doongjohn@gmail.com>
2024-06-25 23:20:18 +0900

    update

--- init.lua
+++ init.lua
@@ -154,21 +154,16 @@ vim.api.nvim_create_autocmd('FileType', {
 -- setup lazy nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- plugins
 require 'lazy'.setup({
-  import = 'plugins'
-}, {
+  spec = {
+    { import = 'plugins' },
+  },
   ui = {
     backdrop = 100,
   },
