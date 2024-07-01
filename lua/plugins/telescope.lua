8a1e632b3f8199412a11b48e7255c384b75bd741
doongjohn <doongjohn@gmail.com>
2024-07-02 03:47:05 +0900

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,7 @@ return {
     'nvim-lua/plenary.nvim',
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-      build =
-      'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
     },
     'natecraddock/telescope-zf-native.nvim',
     'debugloop/telescope-undo.nvim',
