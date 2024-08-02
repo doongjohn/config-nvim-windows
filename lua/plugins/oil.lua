548d3672872d059253f381cedb4ffa458a45a8d3
doongjohn <doongjohn@gmail.com>
2024-08-03 08:53:15 +0900

    update

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,15 +5,44 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   keys = {
-    { '-', function() require('oil').open() end },
+    { '-', function() require 'oil'.open() end },
   },
   opts = {
-    -- delete_to_trash = true, -- windows not supported
+    delete_to_trash = true,
     view_options = {
       show_hidden = true,
       is_always_hidden = function(name, _)
         return vim.startswith(name, '..')
       end,
     },
-  },
+    use_default_keymaps = false,
+    keymaps = {
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["gg"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+        ['yp'] = "actions.yank_entry",
+        ['<c-l>'] = {
+          callback = function()
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'm', false)
+          end,
+          desc = 'ESC',
+          mode = { 'i', 'v' },
+        },
+      },
+    },
+  }
 }
