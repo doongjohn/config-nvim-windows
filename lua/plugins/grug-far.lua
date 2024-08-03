87f616ebcffe108fea0a4bea9e005a78eb2be331
doongjohn <doongjohn@gmail.com>
2024-08-04 00:43:03 +0900

    update

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -1,23 +1,28 @@
 return {
   'magicduck/grug-far.nvim',
   opts = {
-    extraRgArgs =
-      '--no-heading -.ni'
-      .. ' -g !.git'
-      .. ' -g !.github'
-      .. ' -g !*cache'
-      .. ' -g !obj'
-      .. ' -g !.objs'
-      .. ' -g !.deps'
-      .. ' -g !lib'
-      .. ' -g !libs'
-      .. ' -g !bin'
-      .. ' -g !out'
-      .. ' -g !build'
-      .. ' -g !target'
-      .. ' -g !vendor'
-      .. ' -g !dist'
-      .. ' -g !node_modules'
-      .. ' -g !zig-out',
+    engines = {
+      ripgrep = {
+        extraArgs =
+            '--no-heading -.ni'
+            .. ' -g !.git'
+            .. ' -g !.github'
+            .. ' -g !*cache'
+            .. ' -g !obj'
+            .. ' -g !.objs'
+            .. ' -g !.deps'
+            .. ' -g !lib'
+            .. ' -g !libs'
+            .. ' -g !bin'
+            .. ' -g !out'
+            .. ' -g !build'
+            .. ' -g !target'
+            .. ' -g !vendor'
+            .. ' -g !dist'
+            .. ' -g !node_modules'
+            .. ' -g !zig-out',
+      },
+    },
+    wrap = false,
   },
 }
