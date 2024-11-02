ef1cbea739aea22deed1f53775b2d4b17464159d
doongjohn <doongjohn@gmail.com>
2024-11-02 20:36:20 +0900

    update

--- lua/plugins/nvim-lightbulb.lua
+++ lua/plugins/nvim-lightbulb.lua
@@ -3,11 +3,15 @@ return {
   event = 'LspAttach',
   opts = {
     sign = { enabled = false },
-    virtual_text = { enabled = false },
     float = {
       enabled = true,
       hl = 'StatusLine',
     },
     autocmd = { enabled = true },
+    ignore = {
+      clients = {
+        'gopls',
+      }
+    }
   },
 }
