764539edd4024fc25fe909ed6ec8154c7858ab9d
doongjohn <doongjohn@gmail.com>
2024-11-20 01:42:19 +0900

    update

--- init.lua
+++ init.lua
@@ -147,9 +147,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
-		local message =
-			{ { "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" }, { "\nPress any key to exit..." } }
-		vim.api.nvim_echo(message, true, {})
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
 		vim.fn.getchar()
 		os.exit(1)
 	end
