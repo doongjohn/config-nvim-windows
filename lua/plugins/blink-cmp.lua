c3f8fd4c4aa71dc32e4ef3fad8134a70ea353e7f
doongjohn <doongjohn@gmail.com>
2024-11-25 19:25:06 +0900

    update

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,11 +2,6 @@ return {
 	"saghen/blink.cmp",
 	lazy = false,
 	version = "v0.*",
-	dependencies = {
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		{ "saghen/blink.compat", opts = { impersonate_nvim_cmp = true } },
-	},
 	opts = {
 		keymap = {
 			preset = "enter",
@@ -16,25 +11,9 @@ return {
 			use_nvim_cmp_as_default = true,
 		},
 		nerd_font_variant = "normal",
-		accept = {
-			expand_snippet = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-		},
 		sources = {
 			completion = {
-				enabled_providers = { "lsp", "path", "luasnip" },
-			},
-			providers = {
-				luasnip = {
-					name = "luasnip",
-					module = "blink.compat.source",
-					score_offset = -3,
-					opts = {
-						use_show_condition = false,
-						show_autosnippets = true,
-					},
-				},
+				enabled_providers = { "lsp", "path", "snippets" },
 			},
 		},
 		trigger = {
