c6aec16d56597cbaad8b0b3d666c8575ea2ac134
doongjohn <doongjohn@gmail.com>
2024-11-27 03:28:16 +0900

    update

---
+++ lua/plugins/devicons.lua
@@ -0,0 +1,25 @@
+return {
+	"nvim-tree/nvim-web-devicons",
+	opts = {
+		override = {
+			nim = {
+				icon = "󰆥",
+				color = "#f3d400",
+				cterm_color = "220",
+				name = "Nim",
+			},
+			nims = {
+				icon = "󰆥",
+				color = "#f3d400",
+				cterm_color = "220",
+				name = "NimScript",
+			},
+			nimble = {
+				icon = "󰆥",
+				color = "#f3d400",
+				cterm_color = "220",
+				name = "Nimble",
+			},
+		},
+	},
+}
