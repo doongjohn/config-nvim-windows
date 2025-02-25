208f36b8af139db607273f51e496495eb6e7bc0d
doongjohn <doongjohn@gmail.com>
2025-02-25 17:04:35 +0900

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -56,16 +56,14 @@ return {
 		vim.cmd.colorscheme("kanagawa-wave")
 
 		-- semantic highlighting: https://gist.github.com/swarn/fb37d9eefe1bc616c2a7e476c0bc0316
-		vim.api.nvim_set_hl(0, "@lsp.type.comment", { link = "@comment" })
-		vim.api.nvim_set_hl(0, "@lsp.type.keyword", { link = "@keyword" })
 		vim.api.nvim_set_hl(0, "@lsp.type.variable", { link = "@variable" })
 		vim.api.nvim_set_hl(0, "@lsp.typemod.method.readonly.cpp", { link = "@function.method" })
-		vim.api.nvim_set_hl(0, "@lsp.type.keywordLiteral.zig", { link = "@keyword" })
 		vim.api.nvim_set_hl(0, "@lsp.type.builtin.zig", { link = "@type.builtin" })
+		vim.api.nvim_set_hl(0, "@lsp.type.keywordLiteral.zig", { link = "@keyword" })
 		vim.api.nvim_set_hl(0, "@lsp.typemod.namespace.readonly.odin", { link = "@module" })
 		vim.api.nvim_set_hl(0, "@lsp.typemod.type.readonly.odin", { link = "@type" })
-		vim.api.nvim_set_hl(0, "@lsp.typemod.struct.readonly.odin", { link = "@type" })
 		vim.api.nvim_set_hl(0, "@lsp.typemod.enum.readonly.odin", { link = "@type" })
+		vim.api.nvim_set_hl(0, "@lsp.typemod.struct.readonly.odin", { link = "@type" })
 
 		-- https://gist.github.com/swarn/fb37d9eefe1bc616c2a7e476c0bc0316?permalink_comment_id=4534819#dealing-with-ambiguity
 		vim.api.nvim_create_autocmd("LspTokenUpdate", {
