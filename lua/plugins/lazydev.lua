7aecfd232611993aa6a4fe639f9d6dc7e8b2e274
doongjohn <doongjohn@gmail.com>
2025-04-15 05:09:05 +0900

    update

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -5,20 +5,11 @@ return {
 		local lazydev = require("lazydev")
 		local luals_dir = vim.fs.dirname(vim.fs.dirname(vim.fn.exepath("lua-language-server")))
 
-		---@diagnostic disable: missing-fields
 		lazydev.setup({
 			library = {
-				vim.fs.joinpath(luals_dir, "meta/template"),
-				{
-					path = vim.fs.joinpath(luals_dir, "meta/3rd/luv/library"),
-					words = {
-						"vim%.uv",
-						"vim%.schedule",
-						"vim%.defer_fn",
-					},
-				},
+				vim.fs.joinpath(luals_dir, "meta/LuaJIT en-us utf8"),
+				vim.fs.joinpath(luals_dir, "meta/3rd/luv/library"),
 			},
 		})
-		---@diagnostic enable: missing-fields
 	end,
 }
