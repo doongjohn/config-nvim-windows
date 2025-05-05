ebc9f76c66553034c4cf921f0c0948e25f53741d
doongjohn <doongjohn@gmail.com>
2025-05-05 18:46:26 +0900

    update

--- lua/winbar.lua
+++ lua/winbar.lua
@@ -16,9 +16,9 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function()
 		if vim.api.nvim_win_get_config(0).relative ~= "" then
 			-- ignore floating window
-			return
 		elseif vim.startswith(vim.api.nvim_buf_get_name(0), "oil") then
-			vim.opt_local.winbar = [[%#TabLineSel# oil%{&modified ? " *" : ""} %#Comment# %{%luaeval("vim.api.nvim_buf_get_name(0):sub(7,-1)")%}]]
+			vim.opt_local.winbar =
+				[[%#TabLineSel# oil%{&modified ? " *" : ""} %#Comment# %{%luaeval("vim.api.nvim_buf_get_name(0):sub(7,-1)")%}]]
 		elseif not vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
 			vim.opt_local.winbar = [[%#TabLineSel# %t%{&modified ? " *" : ""} %#Comment#]]
 		end
