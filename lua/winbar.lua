4c7fcc2ba1acc298d0fc4f7d5e7cbcf0f2529242
doongjohn <doongjohn@gmail.com>
2025-05-08 20:53:35 +0900

    update

--- lua/winbar.lua
+++ lua/winbar.lua
@@ -10,19 +10,21 @@ local winbar_filetype_exclude = {
 	"NeogitStatus",
 }
 
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
 	group = "config",
 	callback = function()
 		if vim.api.nvim_win_get_config(0).relative ~= "" then
 			-- ignore floating window
-		elseif vim.startswith(vim.api.nvim_buf_get_name(0), "oil") then
+		elseif vim.bo.ft == "oil" then
 			vim.opt_local.winbar = [[%#TabLineSel# oil%{&modified ? " " : ""} ]]
 				.. [[%#LineNr# %{v:lua.Config.oil_get_path()}]]
-		elseif vim.bo.filetype == "SymbolsSidebar" then
+		elseif vim.bo.ft == "SymbolsSidebar" then
 			vim.opt_local.winbar = [[%#TabLineSel# symbols %#Comment#]]
-		elseif not vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
+		elseif not vim.tbl_contains(winbar_filetype_exclude, vim.bo.ft) then
 			vim.opt_local.winbar = [[%#TabLineSel# %t%{&modified ? " " : ""} ]]
 				.. [[%#LineNr# %{v:lua.Config.buf_get_short_path()}]]
+		else
+			vim.opt_local.winbar = ""
 		end
 	end,
 })
