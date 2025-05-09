6d5c1116b385dd90b794649d8b92775e7e383fe3
doongjohn <doongjohn@gmail.com>
2025-05-09 23:31:14 +0900

    update

--- lua/plugins/nvim-bqf.lua
+++ lua/plugins/nvim-bqf.lua
@@ -2,7 +2,12 @@ return {
 	"kevinhwang91/nvim-bqf",
 	ft = "qf",
 	opts = {
+		auto_resize_height = true,
 		preview = {
+			border = "solid",
+			show_title = false,
+			win_height = 12,
+			win_vheight = 12,
 			winblend = 0,
 		},
 	},
