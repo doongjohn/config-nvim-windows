c2362bad6886a361982ea9b7e7f410f352189e0e
doongjohn <doongjohn@gmail.com>
2025-05-09 18:11:48 +0900

    update

--- lua/plugins/symbols.lua
+++ lua/plugins/symbols.lua
@@ -5,6 +5,11 @@ return {
 		local r = require("symbols.recipes")
 		require("symbols").setup(r.DefaultFilters, r.AsciiSymbols, {
 			sidebar = {
+				auto_resize = {
+					enabled = true,
+					min_width = 30,
+					max_width = 40,
+				},
 				show_inline_details = true,
 			},
 		})
