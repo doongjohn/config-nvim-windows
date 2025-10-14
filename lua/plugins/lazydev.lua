eb0071022597143bda574e0f2f1222039918de6d
doongjohn <doongjohn@gmail.com>
2025-10-14 10:56:36 +0900

    update

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,14 +1,9 @@
 return {
 	"folke/lazydev.nvim",
 	ft = "lua",
-	config = function()
-		local lazydev = require("lazydev")
-		local luals_dir = vim.fs.dirname(vim.fs.dirname(vim.fn.exepath("lua-language-server")))
-
-		lazydev.setup({
-			library = {
-				vim.fs.joinpath(luals_dir, "meta/3rd/luv/library"),
-			},
-		})
-	end,
+	opts = {
+		library = {
+			"${3rd}/luv/library",
+		},
+	},
 }
