595bbc0cd3f677947cf216e2501166e233d9a841
doongjohn <doongjohn@gmail.com>
2026-04-26 06:56:44 +0900

    update

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,12 +4,12 @@ return {
 	config = function()
 		local leap = require("leap")
 
-		leap.opts.safe_labels = {}
+		leap.opts.safe_labels = ""
 		leap.opts.vim_opts = {
 			["go.ignorecase"] = true,
 		}
 
-		vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+		vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "DiagnosticInfo" })
 
 		vim.keymap.set("n", "m", "<Plug>(leap-anywhere)")
 		vim.keymap.set("v", "m", "<Plug>(leap)")
